e52ea48ebb43ad089c25f7100237450a028d93f0
Ahmet Affan <ahmet.affan.ebcioglu@outlook.com>
2024-01-28 21:35:15 +0300

    black screen

---
+++ assets/assets.go
@@ -0,0 +1,30 @@
+package assets
+
+import (
+	"embed"
+	"image"
+	_ "image/png"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+//go:embed *
+var Assets embed.FS
+
+var MainChar = GetSingleImage("mainchar.png")
+var LeftChar = GetSingleImage("left.png")
+var RightChar = GetSingleImage("right.png")
+
+func GetSingleImage(name string) *ebiten.Image {
+	file, err := Assets.Open(name)
+	if err != nil {
+		panic(err)
+	}
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+
+	return ebiten.NewImageFromImage(img)
+}
--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/lidldev/GopherJump/game"
+)
+
+func main() {
+	g := game.NewGame()
+
+	if err := ebiten.RunGame(g); err != nil {
+		panic(err)
+	}
+}
